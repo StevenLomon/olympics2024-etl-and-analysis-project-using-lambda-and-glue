f0bf1d6740a301e11d5fb081a9ae5dccab6033be
StevenLomon <steven.lennartsson@gmail.com>
2024-08-27 15:06:37 +0200

    Updated the data extraction Lambda function to implement DynamoDB for automation of page incrementation

--- lambda_function_extract.py
+++ lambda_function_extract.py
@@ -5,8 +5,16 @@ import boto3
 from datetime import datetime
 
 def lambda_handler(event, context):
+    # Initialize DynamoDB client
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('OlympicsAPIState')
+    
+    # Get the current page number from DynamoDB
+    response = table.get_item(Key={'id': 'currentPage'})
+    current_page = response['Item']['page']
+    
     # Get API key
-    api_key = os.environ['API-KEY']
+    api_key = os.environ['API_KEY']
     
     # Define the S3 bucket and object key (filename)
     s3_bucket = "olympics2024-bucket-raw"
@@ -14,7 +22,7 @@ def lambda_handler(event, context):
     
     # Set up API strings
     api_url = "https://olympic-sports-api.p.rapidapi.com/athletes"
-    querystring = {"year":"2024","page":"1"}
+    querystring = {"year":"2024","page": str(current_page)}
     headers = {
     	"x-rapidapi-key": api_key,
     	"x-rapidapi-host": "olympic-sports-api.p.rapidapi.com"
@@ -28,7 +36,16 @@ def lambda_handler(event, context):
     s3 = boto3.client('s3')
     s3.put_object(Bucket=s3_bucket, Key=s3_key, Body=json.dumps(data))
     
+    # Increment the page number and update DynamoDB
+    new_page = current_page + 1
+    table.update_item(
+        Key={'id': 'currentPage'},
+        UpdateExpression='SET page = :val1',
+        ExpressionAttributeValues={':val1': new_page}
+    )
+    
     return {
+        'api_key': api_key,
         'statusCode': 200,
-        'body': json.dumps(f'Data successfully uploaded to {s3_bucket}/{s3_key}')
+        'body': json.dumps(f'Data successfully uploaded to {s3_bucket}/{s3_key}, next page: {new_page}')
     }
